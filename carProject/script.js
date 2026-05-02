27efe01a031693abe21e6afb1e98b06015b84151
advaitjayprakash-beep <advaitjayprakash@gmail.com>
2026-05-02 15:58:12 -0400

    added 3 car cards and fixed bullet points. also changed some images

--- carProject/script.js
+++ carProject/script.js
@@ -1,28 +1,29 @@
 const form = document.getElementById("contact-form");
-const statusText = document.getElementById("status-text");
+const statusText = document.getElementById("contact-status");
 
-const EMAILJS_PUBLIC_KEY = tM574wa3nUb2pO2DI;
-const EMAILJS_SERVICE_ID = service_j2n6d1v;
-const EMAILJS_TEMPLATE_ID = template_lpbvozs;
+const EMAILJS_PUBLIC_KEY = "tM574wa3nUb2pO2DI";
+const EMAILJS_SERVICE_ID = "service_j2n6d1v";
+const EMAILJS_TEMPLATE_ID = "template_lpbvozs";
 
 if (typeof emailjs !== "undefined"){
     emailjs.init({ publicKey: EMAILJS_PUBLIC_KEY });
 }
 
-if (form){
+if (form && statusText){
     form.addEventListener("submit", async function (event) {
-        event.preventDefault;
+        event.preventDefault();
         statusText.textContent = "Sending message...";
         statusText.className = "contact-status sending";
 
-        try{
+        try {
            await emailjs.sendForm(EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, form);
            statusText.textContent = "Message sent!";
-           statusText.classname = "contact-status sent";
+           statusText.className = "contact-status sent";
            form.reset();
         } catch (error) {
             statusText.textContent = "Message failed to send. Please try again";
             statusText.className = "contact-status error";
+            console.error("Emailjs send error:", error);
         }
     });
 }
